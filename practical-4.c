ac2907695bc5a2ac76c8fc9a430016136604acf6
Dhrumil-Desai <142579607+Dhrumil-Desai@users.noreply.github.com>
2024-08-08 21:51:11 +0530

    practical-4.c

--- practical-4.c
+++ practical-4.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
+#include<conio.h>
 #include<ctype.h>
+//Above the ctype.h header file will be used for the header file declares a set of functions to classify (and transform) individual characters.
+//For example, isupper() checks whether a character is uppercase or not.
 
 char stack[100];
 int top = -1;
 
+//Here, I have create mainly 3 functions PUSH,POP,Priority.
+//Push function will be check that if the Stack is empty or not.
+//If the stack will empty then one element will be insert in the stack. Else message shows that Stack is Overflow.
 void push(char x)
 {
     stack[++top] = x;
 }
 
+//Pop fucntion will be check that is their any element available in the Stack or not.
+//If is their any elemnet in the stack then that element will be delete and insert the symbol of any expression sign in the stack. Else message shows that Stack is Underflow.
 char pop()
 {
     if(top == -1)
@@ -17,6 +25,7 @@ char pop()
         return stack[top--];
 }
 
+//Priority will be check that which elements priority has high or low as per Data Sturcture 
 int priority(char x)
 {
     if(x == '(')
